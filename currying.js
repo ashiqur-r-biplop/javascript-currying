89e6b6a8d7c0ecfeca8a7b11d840501f02579a32
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-25 14:20:49 +0600

    write a currying partent function

--- currying.js
+++ currying.js
@@ -1,6 +1,16 @@
-const add = (a, b, c) => {
-  return a + b + c;
-};
+// const add = (a, b, c) => {
+//   return a + b + c;
+// };
 
-const result = add(1, 2, 3);
+// const result = add(1, 2, 3); // add(1)(2)(3)
+// console.log(result);
+
+const add = (a) => {
+  return (b) => {
+    return (c) => {
+      return a + b + c;
+    };
+  };
+};
+const result = add(1)(2)(3);
 console.log(result);
