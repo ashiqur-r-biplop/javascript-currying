242e703307eb70621211d4e7e10ce83469f1f47d
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-25 14:25:09 +0600

    added nested curring function

--- currying.js
+++ currying.js
@@ -8,9 +8,10 @@
 const add = (a) => {
   return (b) => {
     return (c) => {
-      return a + b + c;
+      return a * (b + c);
     };
   };
 };
-const result = add(1)(2)(3);
+const level = add(1);
+const result = level(2)(3)
 console.log(result);
