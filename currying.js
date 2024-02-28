0cdff8a883b9b8deaddfb17dc943b4ac414ecc5c
ashiqur-r-biplop <biplops22@gmail.com>
2024-02-28 19:55:47 +0600

    un comment

--- currying.js
+++ currying.js
@@ -1,9 +1,9 @@
-// const add = (a, b, c) => {
-//   return a + b + c;
-// };
+const add2 = (a, b, c) => {
+  return a + b + c;
+};
 
-// const result = add(1, 2, 3); // add(1)(2)(3)
-console.log(result);
+const result2 = add(1, 2, 3); // add(1)(2)(3)
+console.log(result2);
 
 const add = (a) => {
   return (b) => {
